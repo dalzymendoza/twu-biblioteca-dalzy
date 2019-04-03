59b575fb95d44cd17107804a21a19188491719f6
Dalzy  Mendoza <dmendoza@student.unimelb.edu.au>
2019-04-03 18:59:18 +1100

    [Dalzy] [1.1] Added welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,9 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.print(WELCOME_MESSAGE);
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaAppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void correct_welcome_message() {
+        BibliotecaApp.main(new String[0]);
+        assertThat(outContent.toString(),
+                is("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"));
+    }
+}
